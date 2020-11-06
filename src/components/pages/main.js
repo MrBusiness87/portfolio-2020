34dc37995ed8bd95cb34c86e5808dc55a13e25de
MrBusiness87 <bbartula87@gmail.com>
2020-11-05 22:44:38 -0500

    11/05/2020-Added 'main.js' to pages folder.

---
+++ src/components/pages/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <link rel="icon" href="./assets/links/index.png" />
+      <p>
+        WELCOME<br />TO MY<br />PORTFOLIO<br />
+      </p>
+    </div>
+  )
+}
