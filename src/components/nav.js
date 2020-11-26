e5a4a1dfcda76cce22e9f99480ac93d7324bfc78
MrBusiness87 <bbartula87@gmail.com>
2020-11-26 10:03:08 -0500

    11/26/2020-fixed footer css.

--- src/components/nav.js
+++ src/components/nav.js
@@ -13,25 +13,18 @@ const navImg = {
   width:"50px",
 }
 
-const homeImg = {
-  url: "./assets/links/home.png",
-}
-
 function NavTabs(props) {
   return (
     <p class="navbar">
       <div class="links">
         <a href="#home" onClick={() => props.handlePageChange(PAGE_NAMES.HOME)} title="HOME" className="nav-link">
-        <img src={homeImg} style={navImg} />
+        <img src={require("./assets/links/home.png")} style={navImg} />
         </a>
         <a href="#about" onClick={() => props.handlePageChange(PAGE_NAMES.ABOUT)} title="ABOUT" className="nav-link">
-        <img src={require ("./assets/links/about.png")} style={navImg} />
         </a>
         <a href="#portfolio" onClick={() => props.handlePageChange(PAGE_NAMES.PORTFOLIO)} title="PORTFOLIO" className="nav-link">
-        <img src={require ("./assets/links/portfolio.png")} style={navImg} />
         </a>
         <a href="#node" onClick={() => props.handlePageChange(PAGE_NAMES.NODE)} title="NODE" className="nav-link">
-        <img src={require ("./assets/links/nodejs.png")} style={navImg} />
         </a>
       </div>
     </p>
