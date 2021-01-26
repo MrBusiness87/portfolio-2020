82cda71607a36c2a83bf01ba71aa5b06ec8a7344
MrBusiness87 <bbartula87@gmail.com>
2021-01-26 13:58:09 -0500

    01/26/2020-fixed node link images

--- src/components/pages/node.js
+++ src/components/pages/node.js
@@ -1,23 +1,28 @@
 import React from "react";
 
+import READMEGEN from "../assets/thumbnails/node/READMEGEN thumbnail.png";
+import NOTE from "../assets/thumbnails/node/NOTE thumbnail.png";
+import TEMPLATE from "../assets/thumbnails/node/TEMPLATE thumbnail.png";
+import SQLTRACKER from "../assets/thumbnails/node/TEMPLATE thumbnail.png";
+
 function Node() {
   return (
     <div className="column">
       <br /><br /><br />
         <a href="https://drive.google.com/file/d/1fXtRFc8gkI2u-qZqqKyhHd6V4E-nQUKa/view" target="_blank" rel="noreferrer">
-          <img src={require ("../assets/thumbnails/node/READMEGEN thumbnail.png")} alt="README GENERATOR" />
+          <img src={READMEGEN} alt="README GENERATOR" />
         </a>
         <a href="https://github.com/MrBusiness87/note-taker/blob/master/README.md" alt="NOTE TAKER">
-          <img src={require ("../assets/thumbnails/node/NOTE thumbnail.png")} alt="NOTE-TAKER" />
+          <img src={NOTE} alt="NOTE-TAKER" />
         </a>
         <p className="ball"><br /><br /><br />
         <h2 className="title">NODE<br />PAGE</h2>
         </p>
         <a href="https://github.com/MrBusiness87/Template-Engine/blob/master/README.md" target="_blank" rel="noreferrer">
-          <img src={require ("../assets/thumbnails/node/TEMPLATE thumbnail.png")} alt="TEMPLATE-ENGINE" />
+          <img src={TEMPLATE} alt="TEMPLATE-ENGINE" />
         </a>
         <a href="assets/thumbnails/SQL_Employee_Tracker_Aug 8, 2020 10_47 PM.webm" target="_blank" rel="noreferrer">
-          <img src={require ("../assets/thumbnails/node/SQL_Employee_Tracker_GIF.gif")} alt="EMPLOYEE TRACKER" />
+          <img src={SQLTRACKER} alt="EMPLOYEE TRACKER" />
         </a>
     </div>
   );
