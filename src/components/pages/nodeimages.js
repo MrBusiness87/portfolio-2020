73aa1277a08d37e561098da10920484667c9f4d0
MrBusiness87 <bbartula87@gmail.com>
2021-02-10 01:31:12 -0500

    02/09/2021-working on node images

--- src/components/pages/nodeimages.js
+++ src/components/pages/nodeimages.js
@@ -1,4 +1,17 @@
-export const READMEGEN = () => {return "../assets/thumbnails/node/READMEGEN_thumbnail.png"};
-export const NOTE = () => {return "../assets/thumbnails/node/NOTE_thumbnail.png"};
-export const TEMPLATE = () => {return "../assets/thumbnails/node/TEMPLATE_thumbnail.png"};
-export const SQLTRACKER = () => {return "../assets/thumbnails/node/mysql.jpg"};
+import Rimg from "../assets/thumbnails/node/READMEGEN_thumbnail.png";
+import Nimg from "../assets/thumbnails/node/NOTE_thumbnail.png";
+import Timg from "../assets/thumbnails/node/TEMPLATE_thumbnail.png";
+import Simg from "../assets/thumbnails/node/mysql.jpg";
+
+export const READMEGEN = () => {
+  return Rimg
+};
+export const NOTE = () => {
+  return Nimg
+};
+export const TEMPLATE = () => {
+  return Timg
+};
+export const SQLTRACKER = () => {
+  return Simg
+};
