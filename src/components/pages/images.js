d95b0c89352ad6c07ea9d81a412c22a44721c9fe
MrBusiness87 <bbartula87@gmail.com>
2021-02-09 23:36:54 -0500

    02/09/2021-working on links

--- src/components/pages/images.js
+++ src/components/pages/images.js
@@ -1,4 +1,9 @@
-READMEGEN = "../assets/thumbnails/node/READMEGEN thumbnail.png";
-NOTE = "../assets/thumbnails/node/NOTE thumbnail.png";
-TEMPLATE = "../assets/thumbnails/node/TEMPLATE thumbnail.png";
-SQLTRACKER = "../assets/thumbnails/node/mysql.jpg";
+import React from "react";
+
+function Image() {
+  const THUMB = {
+  READMEGEN = "../assets/thumbnails/node/READMEGEN thumbnail.png";
+  NOTE = "../assets/thumbnails/node/NOTE thumbnail.png";
+  TEMPLATE = "../assets/thumbnails/node/TEMPLATE thumbnail.png";
+  SQLTRACKER = "../assets/thumbnails/node/mysql.jpg";
+}}
